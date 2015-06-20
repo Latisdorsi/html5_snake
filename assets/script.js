1e954c09fb58e08c21530e2fd581d073b1d6426a
Christopher Cuna <chrohn@outlook.com>
2015-06-21 01:01:58 +0800

    Need to have a hittest

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,12 @@
-//Credits to https://css-tricks.com/learn-canvas-snake-game/ for the logic
+//Credits to https://css-tricks.com/learn-canvas-snake-game/ for the tutorial
 var SnakeGame = {};
 
 SnakeGame.game = (function(){ 
   var ctx;
-
   SnakeGame.width = 400;
   SnakeGame.height = 400;
   SnakeGame.blockSize = 10;
-  var frameLength = 500; //new frame every 0.5 seconds
+  var frameLength = 100; //new frame every 0.5 seconds
 
 //Initiate Program
 function init(){
@@ -18,13 +17,17 @@ $canvas.attr('width', SnakeGame.width);
 var canvas = $canvas[0];
 ctx = canvas.getContext('2d'); 
 Snake = SnakeGame.Snake;
+Apple = SnakeGame.Apple;
+Apple.drawApple(ctx);
 gameLoop();
 }
 
 function gameLoop(){
+  ctx.clearRect(0,0,SnakeGame.height, SnakeGame.width)
   Snake.advanceSnake();
   Snake.drawSnake(ctx);
-  setTimeout(gameLoop, frameLength);
+
+ setTimeout(gameLoop, frameLength);
 }
 
 return{
@@ -32,20 +35,50 @@ return{
 };
 })();
 
+
+//Draws the apple
+SnakeGame.Apple = (function(){
+var apple_Position = [];
+apple_Position = [20,20];
+
+function Draw_Apple(ctx){
+ctx.save();
+ctx.fillStyle = '#4682B4';
+ctx.fillStoke = 'black';
+var radius = SnakeGame.blockSize/2;
+var x = SnakeGame.blockSize * Math.round(Math.random() * SnakeGame.width/ SnakeGame.blockSize)  ;
+var y = SnakeGame.blockSize * Math.round(Math.random() * SnakeGame.width/ SnakeGame.blockSize) ;
+ctx.beginPath();
+ctx.arc(x, y,SnakeGame.blockSize/2 ,0,2*Math.PI, true);
+ctx.fill();
+ctx.restore();
+}
+
+return{
+  drawApple : Draw_Apple
+};
+
+
+})();
+
+
+
+
 //Draws the snake
 SnakeGame.Snake = (function(){
 
   var positionArray = [];
-  positionArray.push = ([6,4]);
-  positionArray.push = ([5,4]);
-  positionArray.push = ([4,4]);
+  positionArray.push([6,4]);
+  positionArray.push([5,4]);
+  positionArray.push([4,4]);
+  var direction = 'right';
 
 function DrawSnake(ctx){
   ctx.save();
   ctx.fillStyle = "#cccccc";
-
+    ctx.strokeStyle ="black";
   for (var i = 0; i < positionArray.length; i++){
-    console.log("test"); 
+
     FillSnake(ctx, positionArray[i]);
   }
   ctx.restore();
@@ -55,16 +88,57 @@ function DrawSnake(ctx){
 function FillSnake(ctx, position){
   var x = SnakeGame.blockSize * position[0];
   var y = SnakeGame.blockSize * position[1];
-  ctx.fillRect(x, y, SnakeGame.blockSize, snakeGame.blockSize);
-  
+
+  ctx.fillRect(x, y, SnakeGame.blockSize, SnakeGame.blockSize);
+  ctx.strokeRect(x, y, SnakeGame.blockSize, SnakeGame.blockSize);
+
 }
 
+
 function AdvanceSnake(){
+
+  //moveSnake[0] += 1; //add 1 to the x position 
+
+  $(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+        direction = 'left';
+        break;
+
+        case 38: // up
+        direction = 'up';
+        break;
+
+        case 39: // right
+        direction = 'right';
+        break;
+
+        case 40: // down
+        direction ='down';
+
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  });
   var moveSnake;
-  moveSnake = positionArray.slice();
-  moveSnake[0] += 1; //add 1 to the x position 
+  moveSnake = positionArray[0].slice();
+  if(direction == 'right'){
+  moveSnake[0] += 1;
+  }
+   if(direction == 'left'){
+  moveSnake[0] -= 1;
+  }
+   if(direction == 'up'){
+  moveSnake[1] -= 1;
+  }
+   if(direction == 'down'){
+  moveSnake[1] += 1;
+  }
   positionArray.unshift(moveSnake);
   positionArray.pop();
+ 
 }
 
 
